2de907bab2ee231f875c11b4243befb40d98ae54
Jack Gibson <94558846+jackfgibson@users.noreply.github.com>
2024-04-29 17:56:58 -0400

    Update courses.js

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -1,20 +1,48 @@
 import { Router } from 'express';
+import { courses, tasks } from '../config/mongoCollections.js'
 const router = Router();
 
 router
 .route('/courses')
 .get(async (req, res) => {
-    // getting all courses
+    //get all courses
 
+    const courses = await courses();
+
+    res.json(courses);
+    
 })
+
+
 .post(async (req, res) => {
     // adding a course
-    
+    const {title} = req.body;
+    const course = {
+        title: title,
+        color: "red",
+        tasks: []
+    };
+
+    const courseCollection = await courses();
+    const insertInfo = await courseCollection.insertOne(course);
+
+    if (insertInfo.insertedCount === 0) {
+        throw new Error('Could not add course.');
+    }
+
+    res.json(course);
+
 })
 
 .route('/courses/:id')
 .delete(async (req, res) => {
     // for deleting a course
+
+    const courseCollection = await courses();
+    const deletedCourse = await courseCollection.deleteOne({ _id: ObjectId(req.params.id) });
+    if (deletedCourse.deletedCount === 0) {
+        throw new Error(`Failed to delete course with ID ${req.params.id}.`);
+    }
     
 })
 
